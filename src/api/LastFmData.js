2a240584dcad26a6fb34d3cf8e2dedfe07a16902
parthmangrola <parthmangrola@gmail.com>
2020-07-17 18:09:09 -0700

    :alien:  Added graphql query for lastfm

--- src/api/LastFmData.js
+++ src/api/LastFmData.js
@@ -2,44 +2,38 @@ import React, { useState, useEffect } from "react";
 import { Spinner } from "react-bootstrap";
 import { connect } from "react-redux";
 import { submitForm } from "../actions";
+import { gql, useQuery } from "@apollo/client";
 
-function LastFmData({ username, period }) {
-  const [lfmData, updateLfmData] = useState({});
-
-  useEffect(() => {
-    fetch(
-      `https://ws.audioscrobbler.com/2.0/?method=user.getTopTracks&user=${username}&api_key=${process.env.REACT_APP_API_KEY}&limit=30&period=${period}&format=json`
-    )
-      .then((response) => {
-        if (response.ok) {
-          return response.json();
-        }
-        throw new Error("error");
-      })
-      .then((data) => updateLfmData(data))
-      .catch(() =>
-        updateLfmData({ error: "Whoops! Something went wrong with Last.fm" })
-      );
-  }, []);
-  console.log(lfmData.toptracks);
-  const { error } = lfmData;
-  const tracks = lfmData?.toptracks;
-  if (error) {
-    return <p>error</p>;
+const TOP_TRACKS = gql`
+  query topTracks {
+    topTracks(username: "parth_m", period: "1month") {
+      name
+      artist {
+        name
+      }
+    }
   }
-  if (!tracks) {
+`;
+function LastFmData({ username, period }) {
+  console.log(username);
+  console.log(period);
+  const { loading, error, data } = useQuery(TOP_TRACKS, {
+    variables: { username, period },
+  });
+  console.log(data);
+  if (loading) {
     return (
       <div>
         <Spinner animation="border" variant="success" />
       </div>
     );
   }
+  if (error) {
+    console.log(error);
+    return <p>error</p>;
+  }
+
   return <h1>serenity now</h1>;
-  // return (
-  //   <div>
-  //     <Spinner animation="border" variant="success" />
-  //   </div>
-  // );
 }
 const mapStateToProps = (state) => {
   console.log(state);
--- src/index.js
+++ src/index.js
@@ -5,20 +5,45 @@ import { createStore, applyMiddleware, compose } from "redux";
 import App from "./components/App";
 import reducers from "./reducers";
 import { Helmet } from "react-helmet";
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+import { gql } from "@apollo/client";
 
 import "./index.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(reducers, composeEnhancers(applyMiddleware()));
 
+const client = new ApolloClient({
+  uri: "https://spotify-fm.herokuapp.com/",
+  cache: new InMemoryCache(),
+});
+
+// client
+//   .query({
+//     query: gql`
+//       query topTracks {
+//         topTracks(username: "parth_m", period: "1month") {
+//           name
+//           artist {
+//             name
+//           }
+//         }
+//       }
+//     `,
+//   })
+//   .then((result) => console.log(result.data.topTracks));
+
 ReactDOM.render(
   <Provider store={store}>
     <Helmet
       title="Spotify.FM Playlists"
       meta={[{ property: "og:title", content: "Home" }]}
-      style={{ backgroundColor: "#000" }}
-    ></Helmet>
-    <App />
+    >
+      <style>{"body { background-color: gray; }"}</style>
+    </Helmet>
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </Provider>,
   document.querySelector("#root")
 );
